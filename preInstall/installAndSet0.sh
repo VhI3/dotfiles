3d920056e3509e62d4cd6a4714d46eefc01ea850
VhI3 <48403210+VhI3@users.noreply.github.com>
2021-12-26 17:56:27 +0100

    preInstall

---
+++ preInstall/installAndSet0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "- - - - - - - - - - - - - - - - -"
+echo "    I N S T A L L   B A S I C S  "
+echo "- - - - - - - - - - - - - - - - -"
+echo "- - - - -Intel Microcode- - - - -"
+echo "- - - - - - - - - - - - - - - - -"
+sudo apt install intel-microcode
+echo "- - - - - - - - - - - - - - - - -"
+echo "- - - - - - Git - - - - - - - - -"
+echo "- - - - - - - - - - - - - - - - -"
+sudo apt install git
+echo "- - - - - - - - - - - - - - - - -"
+echo "- - - - - - Ctags - - - - - - - -"
+echo "- - - - - - - - - - - - - - - - -"
+sudo apt install exuberant-ctags
+echo "- - - - - - - - - - - - - - - - -"
+echo "- - - - - - WGET- - - - - - - - -"
+echo "- - - - - - - - - - - - - - - - -"
+sudo apt install wget
+echo "- - - - - - - - - - - - - - - - -"
+echo "- - - - - - CURL- - - - - - - - -"
+echo "- - - - - - - - - - - - - - - - -"
+sudo apt install curl
+cd ~
