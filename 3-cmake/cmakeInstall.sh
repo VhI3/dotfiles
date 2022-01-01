e0ba295f18e874e79249b174054f5a8d8e3728ce
VhI3 <48403210+VhI3@users.noreply.github.com>
2022-01-01 19:32:24 +0100

    Update cmakeInstall.sh

--- 3-cmake/cmakeInstall.sh
+++ 3-cmake/cmakeInstall.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-echo "- - - - - - - - - - - - - - - - -"
-echo "- - - - - Cmake-3.22.1- - - - - -"
-echo "- - - - - - - - - - - - - - - - -"
-sudo apt remove --purge cmake
-sudo apt update
-sudo apt upgrade 
-sudo apt autoremove
-sudo apt install build-essential libssl-dev
-wget https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz -P ~/tmp/
-tar -zxvf ~/tmp/cmake-3.22.1.tar.gz -C ~/tmp/
-rm -rf ~/tmp/cmake-3.22.1.tar.gz
-cd ~/tmp/cmake-3.22.1/
-./bootstrap
-make -j4
-sudo make install
-cd ~
-rm -rf ~/tmp/cmake*
-echo "- - - - - - - - - - - - - - - - -"
-echo "- - - - - - End Cmake - - - - - -"
-echo "- - - - - - - - - - - - - - - - -"
+cmakeInstall=false;
+echo "Do you want to install cmake-3.22.1? (y/n)"
+read -n 1 cmakeInstall ; echo; echo
+if [ $cmakeInstall == "y" ] || [ $cmakeInstall == "Y" ] ; then
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "- - - - - Cmake-3.22.1- - - - - -"
+    echo "- - - - - - - - - - - - - - - - -"
+    sudo apt remove --purge cmake
+    sudo apt update
+    sudo apt upgrade
+    sudo apt autoremove
+    sudo apt install build-essential libssl-dev
+    wget https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz -P ~/tmp/
+    tar -zxvf ~/tmp/cmake-3.22.1.tar.gz -C ~/tmp/
+    rm -rf ~/tmp/cmake-3.22.1.tar.gz
+    cd ~/tmp/cmake-3.22.1/
+    ./bootstrap
+    make -j4
+    sudo make install
+    cd ~
+    rm -rf ~/tmp/cmake*
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "- - - - - - End Cmake - - - - - -"
+    echo "- - - - - - - - - - - - - - - - -"
+fi
