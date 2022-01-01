046280b5189471d2897ee5c75a3d0fc5aa9bacf8
VhI3 <48403210+VhI3@users.noreply.github.com>
2022-01-01 19:00:56 +0100

    add basic installation

--- 1-preInstall/installAndSet0.sh
+++ 1-preInstall/installAndSet0.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-echo "- - - - - - - - - - - - - - - - -"
-echo "    I N S T A L L   B A S I C S  "
-echo "- - - - - - - - - - - - - - - - -"
-echo "- - - - -Intel Microcode- - - - -"
-echo "- - - - - - - - - - - - - - - - -"
-sudo apt install intel-microcode
-echo "- - - - - - - - - - - - - - - - -"
-echo "- - - - - - Git - - - - - - - - -"
-echo "- - - - - - - - - - - - - - - - -"
-sudo apt install git
-echo "- - - - - - - - - - - - - - - - -"
-echo "- - - - - - Ctags - - - - - - - -"
-echo "- - - - - - - - - - - - - - - - -"
-sudo apt install exuberant-ctags
-echo "- - - - - - - - - - - - - - - - -"
-echo "- - - - - - WGET- - - - - - - - -"
-echo "- - - - - - - - - - - - - - - - -"
-sudo apt install wget
-echo "- - - - - - - - - - - - - - - - -"
-echo "- - - - - - CURL- - - - - - - - -"
-echo "- - - - - - - - - - - - - - - - -"
-sudo apt install curl
-cd ~
+basicInstall=false;
+echo "Do you want install some basic software? (y/n)"
+read -n 1 basicInstall ; echo; echo
+if [ $basicInstall == "y" ] || [ $basicInstall == "Y" ] ; then
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "    I N S T A L L   B A S I C S  "
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "- - - - -Intel Microcode- - - - -"
+    echo "- - - - - - - - - - - - - - - - -"
+    sudo apt install intel-microcode
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "- - - - - - Git - - - - - - - - -"
+    echo "- - - - - - - - - - - - - - - - -"
+    sudo apt install git
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "- - - - - - Ctags - - - - - - - -"
+    echo "- - - - - - - - - - - - - - - - -"
+    sudo apt install exuberant-ctags
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "- - - - - - WGET- - - - - - - - -"
+    echo "- - - - - - - - - - - - - - - - -"
+    sudo apt install wget
+    echo "- - - - - - - - - - - - - - - - -"
+    echo "- - - - - - CURL- - - - - - - - -"
+    echo "- - - - - - - - - - - - - - - - -"
+    sudo apt install curl
+fi
--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 clear
 echo "- - - - - - - VHI3- - - - - - - - -"
 echo "/ D O T F I L E S   M A N A G E R \\"
 echo "- - - - - - - - - - - - - - - - - -"
-
 . ~/dotfiles/1-preInstall/installAndSet0.sh
 . ~/dotfiles/2-GCC/gcc.sh
 . ~/dotfiles/3-cmake/cmakeInstall.sh
