391a72a15134882a2b47ad7f68e7aace692ee1fb
VhI3 <48403210+VhI3@users.noreply.github.com>
2022-01-01 18:50:52 +0100

    VScode debug

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+echo "- - - - - - - VHI3- - - - - - - - -"
+echo "/ D O T F I L E S   M A N A G E R \\"
+echo "- - - - - - - - - - - - - - - - - -"
+
+. ~/dotfiles/1-preInstall/installAndSet0.sh
+. ~/dotfiles/2-GCC/gcc.sh
+. ~/dotfiles/3-cmake/cmakeInstall.sh
