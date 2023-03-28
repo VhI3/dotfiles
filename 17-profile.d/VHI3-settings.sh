59a8bc50393a9769517050dc1f5414ca3d01e9c6
VhI3 <48403210+VhI3@users.noreply.github.com>
2023-03-28 06:49:50 +0200

    Update VHI3-settings.sh

--- 17-profile.d/VHI3-settings.sh
+++ 17-profile.d/VHI3-settings.sh
@@ -2,6 +2,6 @@ setxkbmap -option caps:swapescape
 setxkbmap -layout us,ir
 setxkbmap -option 'grp:alt_shift_toggle'
 export PATH="~/.local/bin:$PATH"
-#export PATH="/usr/local/texlive/2022/bin/x86_64-linux:$PATH"
-#export INFOPATH=$INFOPATH:/usr/local/texlive/2022/texmf-dist/doc/info
-#export MANPATH=$MANPATH:/usr/local/texlive/2022/texmf-dist/doc/man
+export PATH="/usr/local/texlive/2022/bin/x86_64-linux:$PATH"
+export INFOPATH="/usr/local/texlive/2023/texmf-dist/doc/info:$INFOPATH"
+export MANPATH="/usr/local/texlive/2023/texmf-dist/doc/info:$MANPATH"
