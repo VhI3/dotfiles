252f1e048dc58429a2ba59bde37374a8aea7ae99
VhI3 <48403210+VhI3@users.noreply.github.com>
2024-06-09 22:37:39 +0200

    update

--- 03-cmake/cmake_Install.sh
+++ 03-cmake/cmake_Install.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-cmakeInstall=false;
-echo "Do you want to install cmake-3.24.0? (y/n)"
-read -n 1 cmakeInstall ; echo; echo
-if [ $cmakeInstall == "y" ] || [ $cmakeInstall == "Y" ] ; then
-    echo "- - - - - - - - - - - - - - - - -"
-    echo "- - - - - Cmake-3.22.1- - - - - -"
-    echo "- - - - - - - - - - - - - - - - -"
-    sudo apt remove --purge cmake
-    sudo apt update
-    sudo apt upgrade
-    sudo apt autoremove
-    sudo apt install build-essential libssl-dev
-    wget https://github.com/Kitware/CMake/releases/download/v3.24.0-rc4/cmake-3.24.0-rc4.tar.gz -P ~/tmp/
-    tar -zxvf ~/tmp/cmake-3.24.0-rc4.tar.gz -C ~/tmp/
-    cd ~/tmp/cmake-3.24.0-rc4/
-    ./bootstrap
-    make -j4
-    sudo make install
-    cd ~
-    rm -rf ~/tmp/cmake*
-    echo "- - - - - - - - - - - - - - - - -"
-    echo "- - - - - - End Cmake - - - - - -"
-    echo "- - - - - - - - - - - - - - - - -"
+cmakeInstall=false
+echo "Do you want to install cmake-3.30.0-rc1? (y/n)"
+read -n 1 cmakeInstall
+echo
+echo
+if [ $cmakeInstall == "y" ] || [ $cmakeInstall == "Y" ]; then
+	echo "- - - - - - - - - - - - - - - - -"
+	echo "- - - - - Cmake-3.30.0-rc1- - - -"
+	echo "- - - - - - - - - - - - - - - - -"
+	sudo apt remove --purge cmake
+	sudo apt update
+	sudo apt upgrade
+	sudo apt autoremove
+	sudo apt install build-essential libssl-dev
+	wget https://github.com/Kitware/CMake/releases/download/v3.30.0-rc1/cmake-3.30.0-rc1.tar.gz -P ~/tmp/
+	tar -zxvf ~/tmp/cmake-3.30.0-rc1.tar.gz -C ~/tmp/
+	cd ~/tmp/cmake-3.30.0-rc1/
+	./bootstrap
+	make -j4
+	sudo make install
+	cd ~
+	rm -rf ~/tmp/cmake*
+	echo "- - - - - - - - - - - - - - - - -"
+	echo "- - - - - - End Cmake - - - - - -"
+	echo "- - - - - - - - - - - - - - - - -"
 fi
+
