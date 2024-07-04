53be47ffbeed53c97535bb2f612add5d5ef04059
VhI3 <48403210+VhI3@users.noreply.github.com>
2024-07-04 10:04:26 +0200

    update

--- 06-i3gaps/i3gaps.sh
+++ 06-i3gaps/i3gaps.sh
@@ -5,17 +5,20 @@ read -n 1 RofiFehInstall
 echo
 echo
 if [ $RofiFehInstall == "y" ] || [ $RofiFehInstall == "Y" ]; then
-    echo "- - - - - - - - - - - - - - - - -"
-    echo "- - - - - - i3WM-GAPS - - - - - -"
-    echo "- - - - - - - - - - - - - - - - -"
-    sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev meson
-    cd ~/tmp
-    git clone https://www.github.com/Airblader/i3 i3-gaps
-    cd i3-gaps
-    meson build
-    sudo ninja -C build/ install
-    cd ~
-    echo "- - - - - - - - - - - - - - - - -"
-    echo "- - - - - - I3LOCK- - - - - - - -"
-    echo "- - - - - - - - - - - - - - - - -"
+  echo "- - - - - - - - - - - - - - - - -"
+  echo "- - - - - - i3WM-GAPS - - - - - -"
+  echo "- - - - - - - - - - - - - - - - -"
+  sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev meson
+  cd ~/tmp
+  git clone https://www.github.com/Airblader/i3 i3-gaps
+  cd i3-gaps
+  meson build
+  sudo ninja -C build/ install
+  cd ~
+  echo "- - - - - - - - - - - - - - - - -"
+  echo "- - - - - - I3LOCK- - - - - - - -"
+  echo "- - - - - - - - - - - - - - - - -"
 fi
+
+# TODO: Check which computer is it based on hostname
+#
