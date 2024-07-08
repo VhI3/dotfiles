36c89d6cf01356927cb8a5bb1e88e17870a3aa96
VhI3 <48403210+VhI3@users.noreply.github.com>
2024-07-08 15:46:22 +0200

    Update githubDesktopUpdate.sh

--- 18-githubDesktop/githubDesktopUpdate.sh
+++ 18-githubDesktop/githubDesktopUpdate.sh
@@ -15,8 +15,8 @@ INSTALLED_VERSION=$(apt list --installed | grep github-desktop | grep -oP 'githu
 
 # Check if the versions were fetched successfully
 if [ -z "$LATEST_VERSION" ] || [ -z "$INSTALLED_VERSION" ]; then
-    echo "Error: Unable to determine the versions."
-    exit 1
+  echo "Error: Unable to determine the versions."
+  exit 1
 fi
 
 # Display the fetched versions
@@ -25,35 +25,35 @@ echo "Installed Version: $INSTALLED_VERSION"
 
 # Compare the fetched versions
 if [ "$LATEST_VERSION" = "$INSTALLED_VERSION" ]; then
-    echo "Your GitHub Desktop is up-to-date."
+  echo "Your GitHub Desktop is up-to-date."
 else
-    echo "A newer version of GitHub Desktop is available."
-    # Ask the user if they wish to install the latest version
-    read -p "Do you want to install the latest version? (y/N) " response
-    if [[ "$response" =~ ^[Yy]$ ]]; then
-        # Fetch the download URL for the .deb file for the latest version
-        DOWNLOAD_URL=$(curl -s $API_URL | jq -r --arg ver "amd64-$LATEST_VERSION.deb" '.assets[] | select(.name | endswith($ver)) | .browser_download_url')
+  echo "A newer version of GitHub Desktop is available."
+  # Ask the user if they wish to install the latest version
+  read -p "Do you want to install the latest version? (y/N) " response
+  if [[ "$response" =~ ^[Yy]$ ]]; then
+    # Fetch the download URL for the .deb file for the latest version
+    DOWNLOAD_URL=$(curl -s $API_URL | jq -r --arg ver "amd64-$LATEST_VERSION.deb" '.assets[] | select(.name | endswith($ver)) | .browser_download_url')
 
-        # Check if the download URL was found
-        if [ -z "$DOWNLOAD_URL" ]; then
-            echo "Error: Download URL not found."
-            exit 1
-        fi
-
-        echo "Downloading GitHub Desktop version $LATEST_VERSION..."
-        # Download the .deb file
-        curl -LO $DOWNLOAD_URL
-
-        # Extract the filename from the URL
-        FILENAME=$(basename $DOWNLOAD_URL)
-
-        # Install the downloaded package
-        echo "Installing GitHub Desktop..."
-        sudo apt install ./$FILENAME
-
-        # Clean up the downloaded file
-        rm $FILENAME
-    else
-        echo "Update cancelled."
+    # Check if the download URL was found
+    if [ -z "$DOWNLOAD_URL" ]; then
+      echo "Error: Download URL not found."
+      exit 1
     fi
+
+    echo "Downloading GitHub Desktop version $LATEST_VERSION..."
+    # Download the .deb file
+    curl -LO $DOWNLOAD_URL
+
+    # Extract the filename from the URL
+    FILENAME=$(basename $DOWNLOAD_URL)
+
+    # Install the downloaded package
+    echo "Installing GitHub Desktop..."
+    sudo apt install ./$FILENAME
+
+    # Clean up the downloaded file
+    rm $FILENAME
+  else
+    echo "Update cancelled."
+  fi
 fi
