6f12c1b538e2ae32506870c394a59a113f20603e
VhI3 <48403210+VhI3@users.noreply.github.com>
2024-08-23 09:26:53 +0200

    changeTheme

--- changeTheme/changeTheme2.sh
+++ changeTheme/changeTheme2.sh
@@ -2,11 +2,17 @@
 
 # Function to set the dark theme
 set_dark_theme() {
+  ln -sf ~/dotfiles/09-alacritty/alacritty_dark.toml ~/.config/alacritty/alacritty.toml
+  ln -sf ~/dotfiles/19-zathura/zathurarc_dark ~/.config/zathura/zathurarc
+  ln -sf ~/dotfiles/nvim/plugins/theme_dark.lua ~/.config/nvim/lua/plugins/theme.lua
   echo "Dark theme applied."
 }
 
 # Function to set the light theme
 set_light_theme() {
+  ln -sf ~/dotfiles/09-alacritty/alacritty_light.toml ~/.config/alacritty/alacritty.toml
+  ln -sf ~/dotfiles/19-zathura/zathurarc_light ~/.config/zathura/zathurarc
+  ln -sf ~/dotfiles/nvim/plugins/theme_light.lua ~/.config/nvim/lua/plugins/theme.lua
   echo "Light theme applied."
 }
 
---
+++ nvim/plugins/theme_dark.lua
@@ -0,0 +1,31 @@
+return {
+	{ "projekt0n/github-nvim-theme" },
+	{ "miikanissi/modus-themes.nvim" },
+	{ "catppuccin/nvim" },
+
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			-- catppuccin/nvim
+			-- colorscheme = "catppuccin-macchiato",
+			-- colorscheme = "catppuccin-latte",
+			-- colorscheme = "catppuccin-frappe",
+			-- colorscheme = "catppuccin-mocha",
+
+			-- colorscheme = "modus_operandi",
+			-- colorscheme = "modus_vivendi",
+
+			-- colorscheme = "github_dark",
+			-- colorscheme = "github_light",
+			-- colorscheme = "github_dark_dimmed",
+			colorscheme = "github_dark_default",
+			-- colorscheme = "github_light_default",
+			-- colorscheme = "github_dark_high_contrast",
+			-- colorscheme = "github_light_high_contrast",
+			-- colorscheme = "github_dark_colorblind",
+			-- colorscheme = "github_light_colorblind",
+			-- colorscheme = "github_dark_tritanopia",
+			-- colorscheme = "github_light_tritanopia",
+		},
+	},
+}
