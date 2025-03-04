789401857ef14c9f7d3f4d9db47626c6c571bc0f
VhI3 <48403210+VhI3@users.noreply.github.com>
2025-03-04 17:47:12 +0100

    Update toggleterm.lua

--- nvim/extensions/toggleterm.lua
+++ nvim/extensions/toggleterm.lua
@@ -1,6 +1,12 @@
 return {
-	"akinsho/toggleterm.nvim",
-	config = function()
-		require("toggleterm").setup()
-	end,
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require("toggleterm").setup()
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "term://*toggleterm#*",
+      callback = function()
+        vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+      end,
+    })
+  end,
 }
